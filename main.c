1483748472cc2236affc9560a7d7ca526ea156a6
UNIQabes <30741110+UNIQabes@users.noreply.github.com>
2024-04-26 08:21:40 +0900

    処理をさらに削ってみた

--- main.c
+++ main.c
@@ -36,9 +36,9 @@ int main()
 	double *xs_new = malloc(sizeof(double) * np);
 	double *ys_new = malloc(sizeof(double) * np);
 	double *zs_new = malloc(sizeof(double) * np);
-	double *vxs_new = malloc(sizeof(double) * np);
-	double *vys_new = malloc(sizeof(double) * np);
-	double *vzs_new = malloc(sizeof(double) * np);
+	// double *vxs_new = malloc(sizeof(double) * np);
+	// double *vys_new = malloc(sizeof(double) * np);
+	// double *vzs_new = malloc(sizeof(double) * np);
 
 	double dt = 1;
 	double G = 1;
@@ -47,47 +47,44 @@ int main()
 	double starttime = second();
 	for (int stepCount = 0; stepCount < stepNum; stepCount++)
 	{
-// printf("pos[0]:(%f,%f,%f) v[0](%f,%f,%f)\n", xs_old[0], ys_old[0], zs_old[0], vxs_old[0], vys_old[0], vzs_old[0]);
-#pragma omp parallel
-		{
+		// printf("pos[0]:(%f,%f,%f) v[0](%f,%f,%f)\n", xs_old[0], ys_old[0], zs_old[0], vxs_old[0], vys_old[0], vzs_old[0]);
+
 #pragma omp parallel for
-			for (int i = 0; i < np; i++) // i番目のvelocityとpositionを求める
+		for (int i = 0; i < np; i++) // i番目のvelocityとpositionを求める
+		{
+			double x = xs_old[i];
+			double y = ys_old[i];
+			double z = zs_old[i];
+
+			double vx_new = 0;
+			double vy_new = 0;
+			double vz_new = 0;
+			for (int j = 0; j < np; j++) // i番目に対して働くv番目の重力を求める。
 			{
-				double x = xs_old[i];
-				double y = ys_old[i];
-				double z = zs_old[i];
-
-				double vx_new = 0;
-				double vy_new = 0;
-				double vz_new = 0;
-#pragma omp parallel for reduction(+ : vx_new, vy_new, vz_new)
-				for (int j = 0; j < np; j++) // i番目に対して働くv番目の重力を求める。
+				if (j == i)
 				{
-					if (j == i)
-					{
-						continue;
-					}
-					double rij = sqrt(pow(xs_old[j] - x, 2) + pow(ys_old[j] - y, 2) + pow(zs_old[j] - z, 2));
-
-					double axij = (-G) * ms[j] / pow(rij, 2) * (x - xs_old[j]) / rij;
-					double ayij = (-G) * ms[j] / pow(rij, 2) * (y - ys_old[j]) / rij;
-					double azij = (-G) * ms[j] / pow(rij, 2) * (z - zs_old[j]) / rij;
-
-					vx_new += axij * dt;
-					vy_new += ayij * dt;
-					vz_new += azij * dt;
+					continue;
 				}
-				vx_new += vxs_old[i];
-				vy_new += vys_old[i];
-				vz_new += vzs_old[i];
+				double rij = sqrt(pow(xs_old[j] - x, 2) + pow(ys_old[j] - y, 2) + pow(zs_old[j] - z, 2));
 
-				xs_new[i] = x + vx_new * dt;
-				ys_new[i] = y + vy_new * dt;
-				zs_new[i] = z + vz_new * dt;
-				vxs_new[i] = vx_new;
-				vys_new[i] = vy_new;
-				vzs_new[i] = vz_new;
+				double axij = (-G) * ms[j] / pow(rij, 2) * (x - xs_old[j]) / rij;
+				double ayij = (-G) * ms[j] / pow(rij, 2) * (y - ys_old[j]) / rij;
+				double azij = (-G) * ms[j] / pow(rij, 2) * (z - zs_old[j]) / rij;
+
+				vx_new += axij * dt;
+				vy_new += ayij * dt;
+				vz_new += azij * dt;
 			}
+			vx_new += vxs_old[i];
+			vy_new += vys_old[i];
+			vz_new += vzs_old[i];
+
+			xs_new[i] = x + vx_new * dt;
+			ys_new[i] = y + vy_new * dt;
+			zs_new[i] = z + vz_new * dt;
+			vxs_old[i] = vx_new;
+			vys_old[i] = vy_new;
+			vzs_old[i] = vz_new;
 		}
 
 		double *temp;
@@ -102,22 +99,10 @@ int main()
 		temp = zs_old;
 		zs_old = zs_new;
 		zs_new = temp;
-
-		temp = vxs_old;
-		vxs_old = vxs_new;
-		vxs_new = temp;
-
-		temp = vys_old;
-		vys_old = vys_new;
-		vys_new = temp;
-
-		temp = vzs_old;
-		vzs_old = vzs_new;
-		vzs_new = temp;
 	}
 
 	double endtime = second();
-	printf("%f\n", endtime - starttime);
+	printf("Time:{%f}\n", endtime - starttime);
 
 	// for (int i = 0; i < 3; i++){printf("%f/", xs_old[i]);}printf("\n");
 
