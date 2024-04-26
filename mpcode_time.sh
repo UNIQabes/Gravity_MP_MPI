82b8d2a22added922767bad9650970d6aa0cc248
UNIQabes <30741110+UNIQabes@users.noreply.github.com>
2024-04-26 20:15:54 +0900

    環境変数を変更できていなかったので修正

--- mpcode_time.sh
+++ mpcode_time.sh
@@ -4,14 +4,15 @@ array=( 1 2 4 8 16 32 64 )
 for i in $array
 do
 	sum=0
-	OMP_NUM_THREADS=$i
-	for v in $(seq 3)
+	export OMP_NUM_THREADS=$i
+	iterC=10
+	for v in $(seq $iterC)
 	do
 		a=$(bin/main_par)
 		sum=$((a+sum))
 	done
-	sum=$((sum/3))
-	echo "$i thread:"$sum
+	sum=$((sum/iterC))
+	echo "$i threads:"$sum
 done
 
 
